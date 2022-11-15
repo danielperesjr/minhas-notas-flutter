6c3a3f1d87716c0053134ecbdd363132e2cc44cd
Daniel Peres Jr <danielperes.junior@gmail.com>
2022-11-15 20:28:15 -0300

    style: implements Scaffold, FloatingActionButton

---
+++ lib/Home.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Minhas Notas"),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.green,
+        foregroundColor: Colors.white,
+        child: Icon(Icons.add),
+        onPressed: (){},
+      ),
+    );
+  }
+}
